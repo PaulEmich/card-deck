658b21f604c48f594541dd9f9a25864dad0732b2
Paul Emich <emich.paul@gmail.com>
2026-03-08 21:26:03 +0100

    Project scaffolding

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,20 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests')
+    ->name('*.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PER-CS' => true,
+        'strict_param' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+        'trailing_comma_in_multiline' => true,
+    ])
+    ->setFinder($finder)
+    ->setRiskyAllowed(true);
